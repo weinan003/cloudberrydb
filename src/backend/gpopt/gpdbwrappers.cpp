428f71518b1ec5f6219eebb030e676ddec62d769
WANG Weinan <wang.wn827@gmail.com>
2025-12-18 14:41:14 +0800

    ORCA: Collect common subqueries SPJ to CTE

--- src/backend/gpopt/gpdbwrappers.cpp
+++ src/backend/gpopt/gpdbwrappers.cpp
@@ -105,6 +105,17 @@ gpdb::BmsNextMember(const Bitmapset *a, int prevbit)
 	return -2;
 }
 
+BOOL
+gpdb::BmsIsMember(int x,Bitmapset *a)
+{
+	GP_WRAP_START;
+	{
+		return bms_is_member(x, a);
+	}
+	GP_WRAP_END;
+	return false;
+}
+
 void *
 gpdb::CopyObject(void *from)
 {
@@ -1361,6 +1372,17 @@ gpdb::ListFreeDeep(List *list)
 	GP_WRAP_END;
 }
 
+void
+gpdb::ListSort(List *list, list_sort_comparator cmp)
+{
+	GP_WRAP_START;
+	{
+		list_sort(list, cmp);
+		return;
+	}
+	GP_WRAP_END;
+}
+
 TypeCacheEntry *
 gpdb::LookupTypeCache(Oid type_id, int flags)
 {
@@ -1459,6 +1481,17 @@ gpdb::MakeVar(Index varno, AttrNumber varattno, Oid vartype, int32 vartypmod,
 		Oid collation = TypeCollation(vartype);
 		return makeVar(varno, varattno, vartype, vartypmod, collation,
 					   varlevelsup);
+	}
+	GP_WRAP_END;
+	return nullptr;
+}
+
+Alias *
+gpdb::MakeAlias(const char *aliasname, List *colnames)
+{
+	GP_WRAP_START;
+	{
+		return makeAlias(aliasname, colnames);
 	}
 	GP_WRAP_END;
 	return nullptr;
@@ -1723,6 +1756,28 @@ gpdb::MutateQueryOrExpressionTree(Node *node, Node *(*mutator)(Node *, void *), 
 	GP_WRAP_START;
 	{
 		return query_or_expression_tree_mutator_wrapper(node, mutator, context, flags);
+	}
+	GP_WRAP_END;
+	return nullptr;
+}
+
+bool
+gpdb::ContainVarsOfLevelOrAbove(Node *node, int levelsup)
+{
+	GP_WRAP_START;
+	{
+		return contain_vars_of_level_or_above(node, levelsup);
+	}
+	GP_WRAP_END;
+	return false;
+}
+
+Expr *
+gpdb::CanonicalizeQual(Expr *quals, bool is_check)
+{
+	GP_WRAP_START;
+	{
+		return canonicalize_qual(quals, is_check);
 	}
 	GP_WRAP_END;
 	return nullptr;
